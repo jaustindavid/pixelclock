9d7aa4b083dc00a7fe0a151472785b01d37e1ed6
Austin David <austin@austindavid.com>
2023-12-27 12:23:30 -0500

    cleaning up timer & adding a stopwatch

--- fraggles/timer.py
+++ fraggles/timer.py
@@ -16,14 +16,14 @@ class Timer:
       self.interval = interval
     else:
       self.interval = timedelta(seconds=interval)
-    self.start = datetime.now()
+    self.start_time = datetime.now()
 
 
   # is this timer expired?  can be tested frequently,
   # returns True not more than once per interval
   def expired(self):
-    if datetime.now() > self.start + self.interval:
-      self.start = datetime.now()
+    if datetime.now() > self.start_time + self.interval:
+      self.start_time = datetime.now()
       return True
     return False
 
@@ -32,12 +32,36 @@ class Timer:
   # useful to synchronize a loop
   def wait(self):
     if not self.expired():
-      remaining = (self.start + self.interval - datetime.now()).total_seconds()
+      remaining = (self.start_time + self.interval - datetime.now()).total_seconds()
       if remaining > 0:
         sleep(remaining)
-      self.start = datetime.now()
+      self.start_time = datetime.now()
 
 
   # opposite of wait(): next call to expired() will return True
   def expire(self):
-    self.start = datetime.now() - self.interval
+    self.start_time = datetime.now() - self.interval
+
+
+'''
+a stopwatch: start(), stop(), and read()
+
+'''
+class Stopwatch:
+  def __init__(self):
+    self.start()
+
+  def start(self):
+    self.start_time = datetime.now()
+    self.elapsed = 0
+
+
+  def stop(self):
+    self.elapsed = (datetime.now() - self.start_time).total_seconds()
+
+
+  def read(self):
+    if self.elapsed:
+      return self.elapsed
+    else:
+      return (datetime.now() - self.start_time).total_seconds()
--- fraggles/timer_test.py
+++ fraggles/timer_test.py
@@ -1,5 +1,5 @@
 import unittest
-from timer import Timer
+from timer import Timer, Stopwatch
 from datetime import timedelta
 from time import sleep
 
@@ -15,6 +15,21 @@ class TestAnt(unittest.TestCase):
     self.assertTrue(t.expired())
     sleep(3.5)
     self.assertTrue(t.expired())
+    
+
+  def test_stopwatch(self):
+    s = Stopwatch()
+    self.assertGreater(s.read(), 0)
+    self.assertLess(s.read(), 1)
+    sleep(1)
+    self.assertGreater(s.read(), 1)
+    s.stop()
+    elapsed = s.read()
+    sleep(0.25)
+    self.assertEqual(s.read(), elapsed)
+    s.start()
+    self.assertLess(s.read(), 1)
+    
     
 
 if __name__ == '__main__':
